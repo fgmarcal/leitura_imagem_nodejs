babb954401c22bf6f7a2a2d42e290e00a7267c88
fgmarcal <felipegarciamarcal@gmail.com>
2024-08-27 11:41:21 -0300

    initial setup

---
+++ prisma/migrations/20240827143541_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "customer_code" TEXT NOT NULL PRIMARY KEY
+);
+
+-- CreateTable
+CREATE TABLE "Measure" (
+    "measure_uuid" TEXT NOT NULL PRIMARY KEY,
+    "measure_datetime" DATETIME NOT NULL,
+    "measure_type" TEXT NOT NULL,
+    "has_confirmed" BOOLEAN NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "customer_code" TEXT NOT NULL,
+    CONSTRAINT "Measure_customer_code_fkey" FOREIGN KEY ("customer_code") REFERENCES "Customer" ("customer_code") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_customer_code_key" ON "Customer"("customer_code");
